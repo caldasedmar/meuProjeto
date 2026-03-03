e2baec1d08b27e6b9aae1274d91a5acfbf2842f0
Edmar Caldas <ecaldas0619@gmail.com>
2026-03-03 18:47:22 -0300

    Removendo token exposto

---
+++ R_git.R
@@ -0,0 +1,35 @@
+
+# https://www.youtube.com/watch?v=fiZStofJqMQ&t=3723s
+# install.packages("usethis")
+# usethis::create_project()
+# criar uma pasta no C:\Documents meuProjeto - apareceu no consolo ~/meuProjeto
+# usethis::use_git()
+# usethis::create_github_token() #logar git hub
+# gitcreds::gitcreds_set()
+# usethis::use_github()  publico
+# usethis::use_github(private = TRUE) nao fiz, mas se fizer so vc pode acessar
+
+
+
+# usethis::use_github()
+
+x <- 1
+y <- 2
+
+soma <- x+y
+soma
+
+multiplicacao <- x*y
+multiplicacao
+
+getwd()
+
+
+# salva o arquivo R_git.R na pasta C:/Users/Edmar/Documents/meuProjeto"
+# Agora no terminal do RStudio (aba Terminal) ou Git Bash: git add R_git.R 
+# agora adiciona o arquivo no git - git commit -m "Adicionando arquivo R_git.R"
+# enviar para git o arquivo git push origin main
+
+
+
+
