da22c1e46492b55d2e08eb2afcc4f5845e1a3957
Edmar Caldas <ecaldas0619@gmail.com>
2026-03-04 19:21:07 -0300

    Alterando em 04/03/26

--- Testando R.R
+++ Testando R.R
@@ -16,6 +16,10 @@ getwd()
 # git add "Testando R.R"
 # git commit -m "Adicionando arquivo Testando R.R"
 # git push origin master
+# git log mostra mostra historico
+# git log --oneline - exibe o histórico de commits de forma compacta, mostrando cada um em uma única linha. 
+# git log --oneline --graph --all -- exibe o histórico de commits de um repositório Git de forma visual, resumida e completa
+# git diff
 
 
 c <- a + b
@@ -28,3 +32,7 @@ e <- c + b
 
 f <- (a + b + c) * 100
 f
+
+g <- c+d+e+f
+g
+
